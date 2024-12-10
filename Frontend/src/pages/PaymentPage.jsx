f1b5adf1fb8f8430fb015e446cf420de5fd2198f
Sawon-52 <mehedihasansawon.52@gmail.com>
2024-12-10 18:49:14 +0600

    created payment page and select the payment option to paypal

--- Frontend/src/pages/PaymentPage.jsx
+++ Frontend/src/pages/PaymentPage.jsx
@@ -1,11 +1,45 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import CheckoutSteps from "../Components/CheckoutSteps";
+import { useNavigate } from "react-router-dom";
+import { savePaymentMethod } from "../slices/cartSlice";
 
 const PaymentPage = () => {
   const [paymentMethod, setPaymentMethod] = useState("Paypal");
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
+  useEffect(() => {
+    if (!shippingAddress) {
+      navigate("/shipping");
+    }
+  }, [shippingAddress, navigate]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(savePaymentMethod(paymentMethod));
+    navigate("/placeorder");
+  };
   return (
     <div>
-        <CheckoutSteps step1 step2 step3 />
+      <CheckoutSteps step1 step2 step3 />
+      <div>
+        <h1 className="text-xl font-bold my-2">Payment Method</h1>
+        <form onSubmit={handleSubmit}>
+          <h2 className="text-base font-medium">Select Method</h2>
+
+          <div className="flex items-center gap-2 my-4">
+            <input type="radio" className="radio w-5 h-5 defaultChecked" name="paymentMethod" value="Paypal-2" onChange={(e) => setPaymentMethod(e.target.value)} />
+            <label htmlFor="">Paypal or Credit Card</label>
+          </div>
+
+          <button type="submit" className="btn bg-primary text-white hover:bg-primary">
+            Continue
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
