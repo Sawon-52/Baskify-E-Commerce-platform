bfdbe6da2fc52249353724401d0ab251f66c2b03
Sawon-52 <mehedihasansawon.52@gmail.com>
2025-01-16 20:46:45 +0600

    changed token secrete

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -14,7 +14,6 @@ const protect = asyncHandler(async (req, res, next) => {
       req.user = await User.findById(decode.userId).select("-password");
       next();
     } catch (error) {
-      console.log(error);
       res.status(401);
       throw new Error("Not Authorized ,Token Failed");
     }
--- Frontend/src/pages/admin/UserEditPage.jsx
+++ Frontend/src/pages/admin/UserEditPage.jsx
@@ -19,15 +19,12 @@ const UserEditPage = () => {
 
   useEffect(() => {
     dispatch(getUser(userId));
-  }, [dispatch, userId]);
-
-  useEffect(() => {
     if (user) {
       setName(user.name);
       setEmail(user.email);
       setIsAdmin(user.isAdmin);
     }
-  }, [user]);
+  }, [dispatch, userId, user]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -41,52 +38,46 @@ const UserEditPage = () => {
     if (result.error) {
       toast.error(result.error);
     } else {
-      toast.success("Product Updated");
+      toast.success("User Updated");
       navigate("/admin/userlist");
     }
   };
 
   return (
     <>
-      {/* <NavLink to={"/admin/userlist"}>
-        <div className="flex items-center text-base gap-1 mb-4">
-          <IoIosArrowRoundBack />
-          <p className="text-sm font-bold">Go Back</p>
-        </div>
-      </NavLink> */}
       {isLoading ? (
         <Loader />
       ) : (
-        <div >
+        <div>
           <h2 className="text-xl font-bold ">Edit User</h2>
           <div className="flex justify-center ">
-          <div className="card w-full max-w-md  p-3 rounded-md">
-            <form onSubmit={handleSubmit}>
-              <div className="form-control">
-                <label className="label font-medium">
-                  <span className="label-text"> Name</span>
-                </label>
-                <input type="text" placeholder="Enter User Name" className="input input-bordered input-sm" value={name} onChange={(e) => setName(e.target.value)} />
-              </div>
-              <div className="form-control">
-                <label className="label font-medium">
-                  <span className="label-text">Email</span>
-                </label>
-                <input type="email" placeholder="Enter User Email" className="input input-bordered input-sm" value={email} onChange={(e) => setEmail(e.target.value)} />
-              </div>
-              <div className="flex gap-1 my-2">
-                <input type="checkbox" className="my-1" checked={isAdmin} onChange={(e) => setIsAdmin(e.target.checked)} />
-                <label className="label font-medium">
-                  <span className="label-text">Is Admin</span>
-                </label>
-              </div>
+            <div className="card w-full max-w-md  p-3 rounded-md">
+              <form onSubmit={handleSubmit}>
+                <div className="form-control">
+                  <label className="label font-medium">
+                    <span className="label-text"> Name</span>
+                  </label>
+                  <input type="text" placeholder="Enter User Name" className="input input-bordered input-sm" value={name} onChange={(e) => setName(e.target.value)} />
+                </div>
+                <div className="form-control">
+                  <label className="label font-medium">
+                    <span className="label-text">Email</span>
+                  </label>
+                  <input type="email" placeholder="Enter User Email" className="input input-bordered input-sm" value={email} onChange={(e) => setEmail(e.target.value)} />
+                </div>
+                <div className="flex gap-1 my-2">
+                  <input type="checkbox" className="my-1" checked={isAdmin} onChange={(e) => setIsAdmin(e.target.checked)} />
+                  <label className="label font-medium">
+                    <span className="label-text">Is Admin</span>
+                  </label>
+                </div>
 
-              <div className="form-control mt-6">
-                <button className="btn  bg-primary text-white hover:bg-black">Update</button>
-              </div>
-            </form>
+                <div className="form-control mt-6">
+                  <button className="btn  bg-primary text-white hover:bg-black">Update</button>
+                </div>
+              </form>
+            </div>
           </div>
-        </div>
         </div>
       )}
     </>
