58c305b576e876cbd75a474bf4673a464202d9d8
Sawon-52 <mehedihasansawon.52@gmail.com>
2025-01-19 07:16:46 +0600

    payment success

--- Backend/controllers/paymentController.js
+++ Backend/controllers/paymentController.js
@@ -66,27 +66,32 @@ const paymentSuccess = asyncHandler(async (req, res) => {
   const { tran_id, val_id, amount, card_type, status, store_amount } = req.body;
 
   if (status === "VALID") {
-    const order = await Order.findOne({
-      paymentResult: { $exists: true, $ne: null },
-      "paymentResult.transactionId": tran_id,
-    });
-
-    if (parseFloat(order?.totalPrice) === parseFloat(amount)) {
-      order.isPaid = true;
-      order.paidAt = Date.now();
-      order.paymentResult = {
-        id: val_id,
-        status,
-        update_time: Date.now(),
-        cardType: card_type,
-        storeAmount: store_amount,
-      };
-      await order.save();
-      // Redirect with query parameters
-      res.redirect(`${process.env.BASE_URL}/orders/${order?._id}?success=true&message=${encodeURIComponent("Payment successful!")}`);
-    } else {
-      res.redirect(`${process.env.BASE_URL}/orders/${order?._id}?success=false&message=${encodeURIComponent("Payment failed. Please try again.")}`);
+    const order = await Order.findOne({ paymentResult: { $exists: true, $ne: null }, "paymentResult.transactionId": tran_id });
+    if (!order) {
+      res.status(404);
+      throw new Error("Order not found");
     }
+    // Verify amount
+    if (parseFloat(order.totalPrice) !== parseFloat(amount)) {
+      res.status(400);
+      throw new Error("Amount mismatch");
+    }
+    // Update order status to paid
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: val_id,
+      status,
+      update_time: Date.now(),
+      cardType: card_type,
+      storeAmount: store_amount,
+    };
+    const updatedOrder = await order.save();
+
+    // Redirect with query parameters
+    res.redirect(`${process.env.BASE_URL}/orders/${order._id}?success=true&message=Payment successful!`);
+  } else {
+    res.redirect(`${process.env.BASE_URL}/?success=false&message=Payment failed. Please try again.`);
   }
 });
 
